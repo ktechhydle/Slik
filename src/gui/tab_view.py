59e4fea3e950a7a7b37dfd6229281bde70cd71a1
kelle <kellerhydle@gmail.com>
2025-05-03 01:26:04 -0600

    UI Updates
    -added start page if all tabs are closed

--- src/gui/tab_view.py
+++ src/gui/tab_view.py
@@ -2,6 +2,23 @@ from src.imports import *
 from src.gui.tab import Tab
 
 
+class StartPage(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setLayout(QVBoxLayout())
+
+        self.createUI()
+
+    def createUI(self):
+        welcome_label = QLabel('<h1>Welcome to Slik!</h1>')
+        help_label = QLabel('To get started, open a file with <code>Ctrl+Q</code>')
+
+        self.layout().addStretch()
+        self.layout().addWidget(welcome_label, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.layout().addWidget(help_label, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.layout().addStretch()
+
+
 class TabView(QTabWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -13,7 +30,7 @@ class TabView(QTabWidget):
 
     def closeTab(self, index: int):
         if self.count() == 1:
-            self.addTab(Tab('', self, Tab.FileTypePython, self), 'New Tab')
+            self.addTab(StartPage(), 'Start')
             self.removeTab(index - 1)
 
         self.removeTab(index)
