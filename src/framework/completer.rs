bcae824bec74c772584cfa4800fdd7deacabad37
Keller Hydle <151480646+ktechhydle@users.noreply.github.com>
2025-05-08 17:04:34 -0600

    Framework Updates
    -working on lsp functions

--- src/framework/completer.rs
+++ src/framework/completer.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 
 
 #[pyfunction]
-pub fn get_completions(code: &str, line: u8, index: u8) -> PyResult<Vec<String>> {
+pub fn get_completions(filename: &str, language: &str, code: &str, line: u8, column: u8) -> PyResult<Vec<&str>> {
 
-    Ok(vec![String::from("fn")])
+    Ok(vec!["test"])
 }
