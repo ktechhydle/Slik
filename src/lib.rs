3e8de5a859ee0b0f1d18180769ebb4aaade3b5c0
kelle <kellerhydle@gmail.com>
2025-05-11 20:56:13 -0600

    Framework Updates
    -renamed rust functions

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,8 @@ fn test() -> PyResult<()> {
 fn slik(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(test, m)?)?;
     m.add_function(wrap_pyfunction!(framework::completer::get_completions, m)?)?;
-    m.add_function(wrap_pyfunction!(framework::file::read, m)?)?;
-    m.add_function(wrap_pyfunction!(framework::file::write, m)?)?;
+    m.add_function(wrap_pyfunction!(framework::file_ops::read, m)?)?;
+    m.add_function(wrap_pyfunction!(framework::file_ops::write, m)?)?;
 
     Ok(())
 }
