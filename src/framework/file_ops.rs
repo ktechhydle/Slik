10a91542e02f240c11d3331b56c89cc7a33f3b02
kelle <kellerhydle@gmail.com>
2025-05-12 23:45:02 -0600

    Project Indexer Updates
    -fixed indexing excluding files starting with "."

--- src/framework/file_ops.rs
+++ src/framework/file_ops.rs
@@ -20,18 +20,14 @@ pub fn write(file_name: &str, contents: &str) -> PyResult<()> {
 pub fn index(dir: &str) -> PyResult<Vec<String>> {
     let mut paths = Vec::new();
 
-    fn is_hidden(entry: &DirEntry) -> bool {
-        // skip any directory or file that starts with .
-        if let Some(name) = entry.file_name().to_str() {
-            name.starts_with(".")
-        } else {
-            false
-        }
+    fn is_hidden_dir(entry: &DirEntry) -> bool {
+        // only ignore directories starting with "."
+        entry.file_type().is_dir() && entry.file_name().to_str().map(|s| s.starts_with(".")).unwrap_or(false)
     }
 
     for entry in WalkDir::new(dir)
         .into_iter()
-        .filter_entry(|e| !is_hidden(e))
+        .filter_entry(|e| !is_hidden_dir(e)) // filter out hidden dirs
         .filter_map(Result::ok)
     {
         if entry.file_type().is_file() {
