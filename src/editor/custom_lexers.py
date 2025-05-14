e5eee1565233bde56f3da57095c0c22e5d72c8f1
kelle <kellerhydle@gmail.com>
2025-05-13 20:00:08 -0600

    Editor Updates
    -updated lexers for markdown
    -refactored code and simplified

--- src/editor/custom_lexers.py
+++ src/editor/custom_lexers.py
@@ -6,6 +6,26 @@ from PyQt6.Qsci import QsciLexerCustom, QsciScintilla, QsciLexerHTML, QsciLexerC
 from PyQt6.QtGui import QColor, QFont
 
 
+class Style:
+    COLOR_BG = QColor('#1e1e1e')
+    COLOR_NORMAL = QColor('#abb2bf')
+    COLOR_COMMENT = QColor('#7f848e')
+    COLOR_KEYWORD = QColor('#c678dd')
+    COLOR_CLASS_DEF = QColor('#e5C07b')
+    COLOR_FUNCTION_DEF = QColor('#61afef')
+    COLOR_BUILTIN = QColor('#56b6c2')
+    COLOR_STRING = QColor('#98c379')
+    COLOR_CONSTANT = QColor('#d19a66')
+    COLOR_LINK = QColor('#5f9bfa')
+    FONT_NORMAL = QFont('JetBrains Mono', 14)
+    FONT_ITALIC = QFont('JetBrains Mono', 14)
+    FONT_ITALIC.setItalic(True)
+    FONT_BOLD = QFont('JetBrains Mono', 14)
+    FONT_BOLD.setBold(True)
+    FONT_UNDERLINE = QFont('JetBrains Mono', 14)
+    FONT_UNDERLINE.setUnderline(True)
+
+
 class BaseLexer(QsciLexerCustom):
     DEFAULT = 0
     KEYWORD = 1
@@ -37,6 +57,9 @@ class BaseLexer(QsciLexerCustom):
 
     def __init__(self, editor: QsciScintilla, language_name: str):
         super().__init__(editor)
+        self.setDefaultColor(QColor('#ffffff'))
+        self.setDefaultPaper(Style.COLOR_BG)
+        self.setDefaultFont(Style.FONT_NORMAL)
 
         self._editor = editor
         self._language_name = language_name
@@ -45,14 +68,6 @@ class BaseLexer(QsciLexerCustom):
         self._builtins = []
         self._types = []
 
-        defaults = {}
-        defaults['color'] = '#ffffff'
-        defaults['paper'] = '#1e1e1e'
-        defaults['font'] = ('JetBrains Mono', 14)
-
-        self.setDefaultColor(QColor(defaults['color']))
-        self.setDefaultPaper(QColor(defaults['paper']))
-        self.setDefaultFont(QFont(defaults['font'][0], defaults['font'][1]))
         self.createStyle()
 
     def language(self):
@@ -63,13 +78,13 @@ class BaseLexer(QsciLexerCustom):
             return 'DEFAULT'
 
         elif style == self.KEYWORD:
-            return 'KEYWORD'
+            return 'COLOR_KEYWORD'
 
         elif style == self.BUILTINS:
             return 'BUILTINS'
 
         elif style == self.STRING:
-            return 'STRING'
+            return 'COLOR_STRING'
 
         elif style == self.KEYARGS:
             return 'kWARGS'
@@ -87,10 +102,10 @@ class BaseLexer(QsciLexerCustom):
             return 'FUNCTIONS'
 
         elif style == self.CLASS_DEF:
-            return 'CLASS_DEF'
+            return 'COLOR_CLASS_DEF'
 
         elif style == self.FUNCTION_DEF:
-            return 'FUNCTION_DEF'
+            return 'COLOR_FUNCTION_DEF'
 
         elif style == self.DECORATOR:
             return 'DECORATOR'
@@ -173,24 +188,19 @@ class PythonLexer(BaseLexer):
         self.setTypes([name for name, obj in vars(builtins).items() if isinstance(obj, type)])
 
     def createStyle(self):
-        normal = QColor('#abb2bf')
-        font = QFont('JetBrains Mono', 14)
-        italic = font
-        italic.setItalic(True)
-
-        self.setFont(italic, PythonLexer.COMMENTS)
-        self.setColor(normal, PythonLexer.DEFAULT)
-        self.setColor(normal, PythonLexer.BRACKETS)
-        self.setColor(QColor(normal), PythonLexer.FUNCTIONS)
-        self.setColor(QColor('#7f848e'), PythonLexer.COMMENTS)
-        self.setColor(QColor('#c678dd'), PythonLexer.KEYWORD)
-        self.setColor(QColor('#e5C07b'), PythonLexer.CLASS_DEF)
-        self.setColor(QColor('#61afef'), PythonLexer.FUNCTION_DEF)
-        self.setColor(QColor('#d19a66'), PythonLexer.DECORATOR)
-        self.setColor(QColor('#56b6c2'), PythonLexer.BUILTINS)
-        self.setColor(QColor('#56b6c2'), PythonLexer.TYPES)
-        self.setColor(QColor('#d19a66'), PythonLexer.CONSTANTS)
-        self.setColor(QColor('#98c379'), PythonLexer.STRING)
+        self.setFont(Style.FONT_ITALIC, PythonLexer.COMMENTS)
+        self.setColor(Style.COLOR_NORMAL, PythonLexer.DEFAULT)
+        self.setColor(Style.COLOR_NORMAL, PythonLexer.BRACKETS)
+        self.setColor(Style.COLOR_NORMAL, PythonLexer.FUNCTIONS)
+        self.setColor(Style.COLOR_COMMENT, PythonLexer.COMMENTS)
+        self.setColor(Style.COLOR_KEYWORD, PythonLexer.KEYWORD)
+        self.setColor(Style.COLOR_CLASS_DEF, PythonLexer.CLASS_DEF)
+        self.setColor(Style.COLOR_FUNCTION_DEF, PythonLexer.FUNCTION_DEF)
+        self.setColor(Style.COLOR_CONSTANT, PythonLexer.DECORATOR)
+        self.setColor(Style.COLOR_BUILTIN, PythonLexer.BUILTINS)
+        self.setColor(Style.COLOR_BUILTIN, PythonLexer.TYPES)
+        self.setColor(Style.COLOR_CONSTANT, PythonLexer.CONSTANTS)
+        self.setColor(Style.COLOR_STRING, PythonLexer.STRING)
 
     def styleText(self, start, end):
         text = self.editor().text()[start:end]
@@ -412,24 +422,19 @@ class RustLexer(BaseLexer):
         ])
 
     def createStyle(self):
-        normal = QColor('#abb2bf')
-        font = QFont('JetBrains Mono', 14)
-        italic = font
-        italic.setItalic(True)
-
-        self.setFont(italic, RustLexer.COMMENTS)
-        self.setColor(normal, RustLexer.DEFAULT)
-        self.setColor(normal, RustLexer.BRACKETS)
-        self.setColor(QColor(normal), RustLexer.FUNCTIONS)
-        self.setColor(QColor('#7f848e'), RustLexer.COMMENTS)
-        self.setColor(QColor('#c678dd'), RustLexer.KEYWORD)
-        self.setColor(QColor('#e5C07b'), RustLexer.CLASS_DEF)
-        self.setColor(QColor('#61afef'), RustLexer.FUNCTION_DEF)
-        self.setColor(QColor('#d19a66'), RustLexer.DECORATOR)
-        self.setColor(QColor('#56b6c2'), RustLexer.BUILTINS)
-        self.setColor(QColor('#56b6c2'), RustLexer.TYPES)
-        self.setColor(QColor('#d19a66'), RustLexer.CONSTANTS)
-        self.setColor(QColor('#98c379'), RustLexer.STRING)
+        self.setFont(Style.FONT_ITALIC, RustLexer.COMMENTS)
+        self.setColor(Style.COLOR_NORMAL, RustLexer.DEFAULT)
+        self.setColor(Style.COLOR_NORMAL, RustLexer.BRACKETS)
+        self.setColor(Style.COLOR_NORMAL, RustLexer.FUNCTIONS)
+        self.setColor(Style.COLOR_COMMENT, RustLexer.COMMENTS)
+        self.setColor(Style.COLOR_KEYWORD, RustLexer.KEYWORD)
+        self.setColor(Style.COLOR_CLASS_DEF, RustLexer.CLASS_DEF)
+        self.setColor(Style.COLOR_FUNCTION_DEF, RustLexer.FUNCTION_DEF)
+        self.setColor(Style.COLOR_CONSTANT, RustLexer.DECORATOR)
+        self.setColor(Style.COLOR_BUILTIN, RustLexer.BUILTINS)
+        self.setColor(Style.COLOR_BUILTIN, RustLexer.TYPES)
+        self.setColor(Style.COLOR_CONSTANT, RustLexer.CONSTANTS)
+        self.setColor(Style.COLOR_STRING, RustLexer.STRING)
 
     def styleText(self, start, end):
         text = self.editor().text()[start:end]
@@ -605,91 +610,102 @@ class HTMLLexer(QsciLexerHTML):
     def __init__(self):
         super().__init__()
         self.setDefaultColor(QColor('#ffffff'))
-        self.setDefaultPaper(QColor('#1e1e1e'))
-        self.setDefaultFont(QFont('JetBrains Mono', 14))
+        self.setDefaultPaper(Style.COLOR_BG)
+        self.setDefaultFont(Style.FONT_NORMAL)
         self.setFoldCompact(False)
 
         self.createStyle()
 
     def createStyle(self):
-        normal = QColor('#abb2bf')
-        font = QFont('JetBrains Mono', 14)
-        italic = QFont('JetBrains Mono', 14)
-        italic.setItalic(True)
-
-        self.setFont(italic, HTMLLexer.HTMLComment)
-        self.setColor(normal, HTMLLexer.Default)
-        self.setColor(QColor('#7f848e'), HTMLLexer.HTMLComment)
+        self.setFont(Style.FONT_ITALIC, HTMLLexer.HTMLComment)
+        self.setColor(Style.COLOR_NORMAL, HTMLLexer.Default)
+        self.setColor(Style.COLOR_COMMENT, HTMLLexer.HTMLComment)
 
 
 class CSSLexer(QsciLexerCSS):
     def __init__(self):
         super().__init__()
         self.setDefaultColor(QColor('#ffffff'))
-        self.setDefaultPaper(QColor('#1e1e1e'))
-        self.setDefaultFont(QFont('JetBrains Mono', 14))
+        self.setDefaultPaper(Style.COLOR_BG)
+        self.setDefaultFont(Style.FONT_NORMAL)
         self.setFoldComments(True)
         self.setFoldCompact(False)
 
         self.createStyle()
 
     def createStyle(self):
-        normal = QColor('#abb2bf')
-        font = QFont('JetBrains Mono', 14)
-        italic = QFont('JetBrains Mono', 14)
-        italic.setItalic(True)
-
-        self.setFont(italic, CSSLexer.Comment)
-        self.setFont(font, CSSLexer.Tag)
-        self.setColor(normal, CSSLexer.Default)
-        self.setColor(normal, CSSLexer.Operator)
-        self.setColor(normal, CSSLexer.CSS1Property)
-        self.setColor(normal, CSSLexer.CSS2Property)
-        self.setColor(normal, CSSLexer.CSS3Property)
-        self.setColor(normal, CSSLexer.UnknownProperty)
-        self.setColor(QColor('#7f848e'), CSSLexer.Comment)
-        self.setColor(QColor('#c678dd'), CSSLexer.Tag)
-        self.setColor(QColor('#d19a66'), CSSLexer.Value)
+        self.setFont(Style.FONT_NORMAL, CSSLexer.Tag)
+        self.setFont(Style.FONT_ITALIC, CSSLexer.Comment)
+        self.setColor(Style.COLOR_NORMAL, CSSLexer.Default)
+        self.setColor(Style.COLOR_NORMAL, CSSLexer.Operator)
+        self.setColor(Style.COLOR_NORMAL, CSSLexer.CSS1Property)
+        self.setColor(Style.COLOR_NORMAL, CSSLexer.CSS2Property)
+        self.setColor(Style.COLOR_NORMAL, CSSLexer.CSS3Property)
+        self.setColor(Style.COLOR_NORMAL, CSSLexer.UnknownProperty)
+        self.setColor(Style.COLOR_COMMENT, CSSLexer.Comment)
+        self.setColor(Style.COLOR_KEYWORD, CSSLexer.Tag)
+        self.setColor(Style.COLOR_CONSTANT, CSSLexer.Value)
         self.setColor(QColor('#e06c75'), CSSLexer.Variable)
-        self.setColor(QColor('#56b6c2'), CSSLexer.IDSelector)
-        self.setColor(QColor('#56b6c2'), CSSLexer.UnknownPseudoClass)
-        self.setColor(QColor('#56b6c2'), CSSLexer.PseudoClass)
-        self.setColor(QColor('#98c379'), CSSLexer.DoubleQuotedString)
-        self.setColor(QColor('#98c379'), CSSLexer.SingleQuotedString)
+        self.setColor(Style.COLOR_BUILTIN, CSSLexer.IDSelector)
+        self.setColor(Style.COLOR_BUILTIN, CSSLexer.UnknownPseudoClass)
+        self.setColor(Style.COLOR_BUILTIN, CSSLexer.PseudoClass)
+        self.setColor(Style.COLOR_STRING, CSSLexer.DoubleQuotedString)
+        self.setColor(Style.COLOR_STRING, CSSLexer.SingleQuotedString)
 
 
 class MarkdownLexer(QsciLexerMarkdown):
     def __init__(self):
         super().__init__()
         self.setDefaultColor(QColor('#ffffff'))
-        self.setDefaultPaper(QColor('#1e1e1e'))
-        self.setDefaultFont(QFont('JetBrains Mono', 14))
+        self.setDefaultPaper(Style.COLOR_BG)
+        self.setDefaultFont(Style.FONT_NORMAL)
 
         self.createStyle()
 
     def createStyle(self):
-        normal = QColor('#abb2bf')
-        font = QFont('JetBrains Mono', 14)
-        italic = QFont('JetBrains Mono', 14)
-        italic.setItalic(True)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.Default)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.Header1)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.Header2)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.Header3)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.Header4)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.Header5)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.EmphasisAsterisks)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.StrongEmphasisAsterisks)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.CodeBlock)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.CodeBackticks)
+        self.setFont(Style.FONT_NORMAL, MarkdownLexer.CodeDoubleBackticks)
+        self.setFont(Style.FONT_ITALIC, MarkdownLexer.EmphasisUnderscores)
+        self.setFont(Style.FONT_ITALIC, MarkdownLexer.StrongEmphasisUnderscores)
+        self.setFont(Style.FONT_ITALIC, MarkdownLexer.StrikeOut)
+        self.setFont(Style.FONT_UNDERLINE, MarkdownLexer.Link)
+        self.setFont(Style.FONT_BOLD, MarkdownLexer.HorizontalRule)
+        self.setColor(Style.COLOR_NORMAL, MarkdownLexer.Default)
+        self.setColor(Style.COLOR_NORMAL, MarkdownLexer.CodeBlock)
+        self.setColor(Style.COLOR_NORMAL, MarkdownLexer.CodeBackticks)
+        self.setColor(Style.COLOR_NORMAL, MarkdownLexer.CodeDoubleBackticks)
+        self.setColor(Style.COLOR_NORMAL, MarkdownLexer.HorizontalRule)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.Header1)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.Header2)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.Header3)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.Header4)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.Header5)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.EmphasisAsterisks)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.StrongEmphasisAsterisks)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.EmphasisUnderscores)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.StrongEmphasisUnderscores)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.BlockQuote)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.OrderedListItem)
+        self.setColor(Style.COLOR_CONSTANT, MarkdownLexer.UnorderedListItem)
+        self.setColor(Style.COLOR_LINK, MarkdownLexer.Link)
 
-        self.setFont(font, MarkdownLexer.Default)
-        self.setFont(font, MarkdownLexer.Header1)
-        self.setFont(font, MarkdownLexer.Header2)
-        self.setFont(font, MarkdownLexer.Header3)
-        self.setFont(font, MarkdownLexer.Header4)
-        self.setFont(font, MarkdownLexer.Header5)
-        self.setFont(font, MarkdownLexer.CodeBlock)
-        self.setFont(italic, MarkdownLexer.EmphasisUnderscores)
-        self.setFont(italic, MarkdownLexer.StrongEmphasisUnderscores)
-        self.setColor(normal, MarkdownLexer.Default)
-        self.setColor(QColor('#d19a66'), MarkdownLexer.Header1)
-        self.setColor(QColor('#d19a66'), MarkdownLexer.Header2)
-        self.setColor(QColor('#d19a66'), MarkdownLexer.Header3)
-        self.setColor(QColor('#d19a66'), MarkdownLexer.Header4)
-        self.setColor(QColor('#d19a66'), MarkdownLexer.Header5)
-        self.setColor(QColor('#d19a66'), MarkdownLexer.EmphasisUnderscores)
-        self.setColor(QColor('#d19a66'), MarkdownLexer.StrongEmphasisUnderscores)
+        # ensure the -, `, and > don't have a background
+        self.setPaper(Style.COLOR_BG, MarkdownLexer.HorizontalRule)
+        self.setPaper(Style.COLOR_BG, MarkdownLexer.OrderedListItem)
+        self.setPaper(Style.COLOR_BG, MarkdownLexer.UnorderedListItem)
+        self.setPaper(Style.COLOR_BG, MarkdownLexer.BlockQuote)
+        self.setPaper(Style.COLOR_BG, MarkdownLexer.CodeBlock)
+        self.setPaper(Style.COLOR_BG, MarkdownLexer.CodeBackticks)
+        self.setPaper(Style.COLOR_BG, MarkdownLexer.CodeDoubleBackticks)
 
 
 class PlainTextLexer(BaseLexer):
@@ -697,9 +713,7 @@ class PlainTextLexer(BaseLexer):
         super().__init__(editor, 'Plain Text')
 
     def createStyle(self):
-        normal = QColor('#ffffff')
-
-        self.setColor(normal, PlainTextLexer.DEFAULT)
+        self.setColor(Style.COLOR_NORMAL, PlainTextLexer.DEFAULT)
 
     def styleText(self, start, end):
         self.startStyling(start)
