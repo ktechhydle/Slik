c0215a4fa79aa5af759a61147aba66a9e28863ef
kelle <kellerhydle@gmail.com>
2025-05-23 21:22:38 -0600

    Lsp Updates
    -more implementations

--- src/framework/language_server/lsp.rs
+++ src/framework/language_server/lsp.rs
@@ -1,39 +1,76 @@
-use lsp_types;
 use pyo3::prelude::*;
+use serde::{self, Deserialize, Serialize};
+use serde_json::json;
+
+const JSON_RPC_VERSION: &str = "2.0";
+
+#[derive(Serialize, Deserialize)]
+enum RequestID {
+    Completion(i32),
+    Definition(i32),
+    Usage(i32),
+    Default(i32),
+}
 
 #[pyclass]
 pub struct LanguageServer;
 
 #[pymethods]
 impl LanguageServer {
+    // Basic language server communication built in rust, wrapped to python
+    // Request IDs:
+    // 1 - Completions
+    // 2 - Definitions
+    // 3 - Usages
     #[new]
     fn new() -> Self {
         LanguageServer {}
     }
 
-    pub fn start(&self) {}
-
-    pub fn open_document(&self, file: &str) -> PyResult<()> {
+    // Start the lsp command for the document (pyright for .py, rust-analyzer for .rs)
+    // This will send a 'textDocument/didOpen' request to the server
+    pub fn open_document(&self, filename: &str) -> PyResult<()> {
         Ok(())
     }
 
-    pub fn close_document(&self, file: &str) -> PyResult<()> {
+    // Stop the lsp command for the specified file
+    // This will send a 'textDocument/didClose' request to the server
+    pub fn close_document(&self, filename: &str) -> PyResult<()> {
         Ok(())
     }
 
+    // Get code completitions for the document
+    // This will send a 'textDocument/completion' request to the server
     pub fn get_document_completions(
         &self,
-        file: &str,
+        filename: &str,
         code: &str,
         line: usize,
         column: usize,
     ) -> PyResult<Vec<String>> {
-        Ok(vec!["greet(name: str)".to_string()])
+        /*let request = json!({
+            "jsonrpc": JSON_RPC_VERSION,
+            "id": RequestID::Completion(1),
+            "method": "textDocument/completion",
+            "params": {
+                "textDocument": {
+                    "uri": format!("file://{}", filename)
+                },
+                "position": {
+                    "line": line,
+                    "character": column
+                }
+            }
+        });*/
+
+        Ok(vec!["".to_string()])
     }
 
+    // Get definitions for the document (based on position)
+    // This will send a 'textDocument/definition' request to the server
     pub fn get_document_definitions(
         &self,
-        file: &str,
+        filename: &str,
         code: &str,
         line: usize,
         column: usize,
@@ -41,29 +78,11 @@ impl LanguageServer {
         Ok(())
     }
 
+    // Get usages for the document (based on position)
+    // This will send a 'textDocument/references' request to the server
     pub fn get_document_usages(
         &self,
-        file: &str,
-        code: &str,
-        line: usize,
-        column: usize,
-    ) -> PyResult<()> {
-        Ok(())
-    }
-
-    pub fn format_document(
-        &self,
-        file: &str,
-        code: &str,
-        line: usize,
-        column: usize,
-    ) -> PyResult<()> {
-        Ok(())
-    }
-
-    pub fn lint_document(
-        &self,
-        file: &str,
+        filename: &str,
         code: &str,
         line: usize,
         column: usize,
