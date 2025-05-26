7f2a8962bd88ba79412cb1d442981627cd7667af
kelle <kellerhydle@gmail.com>
2025-05-25 19:51:39 -0600

    Lsp Updates
    -working on implementations🙄

--- src/framework/language_server/lsp.rs
+++ src/framework/language_server/lsp.rs
@@ -1,64 +1,77 @@
 use pyo3::prelude::*;
-use serde::{self, Deserialize, Serialize};
 use serde_json::json;
+use std::{
+    collections::HashMap,
+    io::BufReader,
+    process::{Child, ChildStdin, ChildStdout, Command, Stdio},
+};
 
 const JSON_RPC_VERSION: &str = "2.0";
 
-#[derive(Serialize, Deserialize)]
-enum RequestID {
-    Completion(i32),
-    Definition(i32),
-    Usage(i32),
-    Document(i32),
+fn as_uri(filename: &str) -> String {
+    let result = format!("file://{}", filename);
+
+    result.to_string()
 }
 
 #[pyclass]
-pub struct LanguageServer;
+pub struct LanguageServer {
+    rust_lsp_command: &'static str,
+    python_lsp_command: &'static str,
+}
 
 #[pymethods]
 impl LanguageServer {
     // Basic language server communication built in rust, wrapped to python
-    // Request IDs:
-    // 1 - Completions
-    // 2 - Definitions
-    // 3 - Usages
-    // 4 - Document Opened
-    // 5 - Document Closed
+    // Starts both a rust-analyzer and pyright-langserver command to communicate with depending on the language
+    // This will essentially send one request, then retrieve results. No async, no threading, one request at a time.
     #[new]
     fn new() -> Self {
-        LanguageServer {}
+        let python_command = Command::new("node_modules/.bin/pyright-langserver.cmd")
+            .arg("--stdio")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn();
+
+        let python_writer = python_command.stdin.take().unwrap();
+        let python_reader = BufReader::new(python_command.stdout.take().unwrap());
+
+        LanguageServer {
+            rust_lsp_command: "rust-analyzer",
+            python_lsp_command: "node_modules/.bin/pyright-langserver.cmd",
+        }
     }
 
     // Start the lsp command for the document (pyright for .py, rust-analyzer for .rs)
     // This will send a 'textDocument/didOpen' request to the server
     pub fn open_document(&self, filename: &str) -> PyResult<()> {
-        /*let request = json!({
+        let request = json!({
             "jsonrpc": JSON_RPC_VERSION,
-            "id": RequestID::Document(4),
+            "id": 1,
             "method": "textDocument/didOpen",
             "params": {
                 "textDocument": {
-                    "uri": format!("file://{}", filename)
+                    "uri": as_uri(filename)
                 }
             }
-        });*/
+        });
 
         Ok(())
     }
 
-    // Stop the lsp command for the specified file
+    // Stop the language server commands for the specified file
     // This will send a 'textDocument/didClose' request to the server
     pub fn close_document(&self, filename: &str) -> PyResult<()> {
-        /*let request = json!({
+        let request = json!({
             "jsonrpc": JSON_RPC_VERSION,
-            "id": RequestID::Document(5),
+            "id": 1,
             "method": "textDocument/didClose",
             "params": {
                 "textDocument": {
-                    "uri": format!("file://{}", filename)
+                    "uri": as_uri(filename)
                 }
             }
-        });*/
+        });
 
         Ok(())
     }
@@ -72,49 +85,53 @@ impl LanguageServer {
         line: usize,
         column: usize,
     ) -> PyResult<Vec<String>> {
-        /*let request = json!({
+        let request = json!({
             "jsonrpc": JSON_RPC_VERSION,
-            "id": RequestID::Completion(1),
+            "id": 1,
             "method": "textDocument/completion",
             "params": {
                 "textDocument": {
-                    "uri": format!("file://{}", filename)
+                    "uri": as_uri(filename)
                 },
                 "position": {
                     "line": line,
                     "character": column
                 }
             }
-        });*/
+        });
 
         Ok(vec!["".to_string()])
     }
 
     // Get definitions for the document (based on position)
     // This will send a 'textDocument/definition' request to the server
+    // Returns HashMap(filename, (line, column))
     pub fn get_document_definitions(
         &self,
         filename: &str,
         code: &str,
         line: usize,
         column: usize,
-    ) -> PyResult<()> {
-        /*let request = json!({
+    ) -> PyResult<HashMap<String, (usize, usize)>> {
+        let request = json!({
             "jsonrpc": JSON_RPC_VERSION,
             "id": RequestID::Definition(2),
             "method": "textDocument/definition",
             "params": {
                 "textDocument": {
-                    "uri": format!("file://{}", filename)
+                    "uri": as_uri(filename)
                 },
                 "position": {
                     "line": line,
                     "character": column
                 }
             }
-        });*/
+        });
 
-        Ok(())
+        let mut results = HashMap::new();
+        results.insert("example.txt".to_string(), (line, column));
+
+        Ok(results)
     }
 
     // Get usages for the document (based on position)
@@ -126,20 +143,20 @@ impl LanguageServer {
         line: usize,
         column: usize,
     ) -> PyResult<()> {
-        /*let request = json!({
+        let request = json!({
             "jsonrpc": JSON_RPC_VERSION,
             "id": RequestID::Usage(3),
             "method": "textDocument/references",
             "params": {
                 "textDocument": {
-                    "uri": format!("file://{}", filename)
+                    "uri": as_uri(filename)
                 },
                 "position": {
                     "line": line,
                     "character": column
                 }
             }
-        });*/
+        });
 
         Ok(())
     }
